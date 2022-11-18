3e86f55200238b61398da5bd7b987a4c2819e6c4
Quesorraerestu <113881206+Quesorraerestu@users.noreply.github.com>
2022-11-18 04:41:47 -0500

    Add files via upload

--- assets/preload/scripts/script.lua
+++ assets/preload/scripts/script.lua
@@ -1,2 +1,19 @@
-var sus = true
-// you can remplace this script with anything else
+function onBeatHit()
+    -- triggered 2 times per section
+    if curBeat % 1 == 0 then
+        setProperty('iconP2.angle', 20);
+        doTweenAngle('bip', 'iconP2', 0,0.2,'cubicOut');
+
+        setProperty('iconP1.angle', 20);
+        doTweenAngle('bop', 'iconP1', 0,0.2,'cubicOut');
+    end
+    -- triggered 4 times per section
+    if curBeat % 2 == 0 then
+        setProperty('iconP2.angle', -20);
+        doTweenAngle('bip', 'iconP2', 0,0.2,'cubicOut');
+
+        setProperty('iconP1.angle', -20);
+        doTweenAngle('bop', 'iconP1', 0,0.2,'cubicOut');
+    end
+    --script made by Gazozoz
+end
